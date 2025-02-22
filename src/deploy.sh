cfd9675b92573018e0233142586b3580dc7cb362
Haki <113508167+kieuhaiha@users.noreply.github.com>
2025-02-22 14:16:23 +0300

    Update deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,5 +1,5 @@
 if scp cat/s21_cat grep/s21_grep hakearie@172.24.116.8:~/ ; then
-        ssh hakearie@172.24.116.8 "echo '123' | sudo -S mv s21_cat s21_grep /usr/local/bin"
+        ssh hakearie@172.24.116.8 "echo $hakearie_PASSWORD | sudo -S mv s21_cat s21_grep /usr/local/bin"
         echo -e "\e[32mSuccesfully copy artifacts to /usr/local/bin\e[0m"
     else
         echo -e "\e[31mCAN'T COPY ARTIFACTS\e[0m"
